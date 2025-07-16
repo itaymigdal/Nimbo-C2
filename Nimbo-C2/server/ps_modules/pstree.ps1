46cdde8ed6dd769327c13544c60aa2f7d2b6b31e
Itay Migdal <32266629+itaymigdal@users.noreply.github.com>
2025-07-16 08:15:07 +0300

    improved pstree

--- Nimbo-C2/server/ps_modules/pstree.ps1
+++ Nimbo-C2/server/ps_modules/pstree.ps1
@@ -1,29 +1,40 @@
-function Get-ProcessTree
-{
+function Get-Bitness {
+    param ([System.Diagnostics.Process]$Process)
+    $modules = $process.modules
+    foreach ($module in $modules) {
+        $file = [System.IO.Path]::GetFileName($module.FileName).ToLower()
+        if ($file -eq "wow64.dll") {
+            return "x32"
+        } 
+    }
+    return "x64"
+}
+
+function Get-ProcessTree {
     [CmdletBinding()]
     param([int]$IndentSize = 2)
     
-    $indentSize   = [Math]::Max(1, [Math]::Min(12, $indentSize))
-    $processes    = Get-WmiObject Win32_Process
-    $pids         = $processes | select -ExpandProperty ProcessId
-    $parents      = $processes | select -ExpandProperty ParentProcessId -Unique
-    $liveParents  = $parents | ? { $pids -contains $_ }
-    $deadParents  = Compare-Object -ReferenceObject $parents -DifferenceObject $liveParents `
-                  | select -ExpandProperty InputObject
+    $indentSize = [Math]::Max(1, [Math]::Min(12, $indentSize))
+    $processes = Get-WmiObject Win32_Process
+    $pids = $processes | select -ExpandProperty ProcessId
+    $parents = $processes | select -ExpandProperty ParentProcessId -Unique
+    $liveParents = $parents | ? { $pids -contains $_ }
+    $deadParents = Compare-Object -ReferenceObject $parents -DifferenceObject $liveParents `
+    | select -ExpandProperty InputObject
     $processByParent = $processes | Group-Object -AsHashTable ParentProcessId
 
-    function Write-ProcessTree($process, [int]$level = 0)
-    {
+    function Write-ProcessTree($process, [int]$level = 0) {
         $id = $process.ProcessId
         $parentProcessId = $process.ParentProcessId
         if ($IsElevated)
-            {$process = Get-Process -Id $id -IncludeUserName}
+        { $process = Get-Process -Id $id -IncludeUserName }
         else
-            {$process = Get-Process -Id $id}
+        { $process = Get-Process -Id $id }
         $indent = New-Object String(' ', ($level * $indentSize))
         $process `
         | Add-Member NoteProperty Process_Id $process.id -PassThru `
-        | Add-Member NoteProperty Process_Name "`t$indent$($process.Name).exe" -PassThru 
+        | Add-Member NoteProperty Process_Name "`t$indent$($process.Name).exe" -PassThru `
+        | Add-Member NoteProperty Bitness "$((Get-Bitness $process))" -PassThru 
 
         $processByParent.Item($id) `
         | ? { $_ } `
@@ -38,6 +49,6 @@ function Get-ProcessTree
 $IsElevated = (new-object System.Security.Principal.WindowsPrincipal([System.Security.Principal.WindowsIdentity]::GetCurrent())).IsInRole("Administrators")
 
 if ($IsElevated) 
-    {Get-ProcessTree -Verbose | select Process_Id, Process_Name, UserName}
+{ Get-ProcessTree -Verbose | select Process_Id, Process_Name, UserName, Bitness }
 else 
-    {Get-ProcessTree -Verbose | select Process_Id, Process_Name}
+{ Get-ProcessTree -Verbose | select Process_Id, Process_Name, Bitness }
